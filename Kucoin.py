cc7bd69b5aa78617c3ba7dd478909b6c82ac600c
_erfanvahdat_ <96780752+erfanvahdat@users.noreply.github.com>
2023-08-19 11:59:51 +0330

    Create Kucoin.py

---
+++ Kucoin.py
@@ -0,0 +1,15 @@
+
+def kucoin(symbol,start,end,interval=None):
+    
+    start=datetime.datetime.timestamp(datetime.datetime.strptime(start,'%Y-%m-%d'))
+    end=datetime.datetime.timestamp(datetime.datetime.strptime(end,'%Y-%m-%d'))
+
+    # &startAt={int(start)}&startAt={int(end)}
+    data=requests.get(f'https://api.kucoin.com/api/v1/market/candles?type=3min&symbol={symbol}-USDT&startAt={int(start)}').json()
+    data=pd.DataFrame(data)['data']
+    data=pd.DataFrame(list(data))
+    data.columns=['time','Open','Close','High','Low','Volume','Closetime',]
+    data.index=[datetime.datetime.fromtimestamp(int(i))   for i in data.loc[:,'time'] ]
+    data=data.drop(['time'],axis=1)
+    data=data.iloc[::-1]
+    return data
